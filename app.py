213ee29daebb15cde2a3d4d7cdc861c5816a03dd
K <kris@gmail.com>
2019-12-12 18:59:28 +0100

    Added app.py 4

--- app.py
+++ app.py
@@ -13,5 +13,9 @@ def login():
 def new_account():
 	return render_template('new_account.html')
 
+@app.route("/")
+def new_feature():
+	return render_template('new_feature.html')
+
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=5000, debug=True)
