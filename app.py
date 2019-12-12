e490771e0cbfb65645390cb1d92ed919701136c4
K <kris@gmail.com>
2019-12-12 18:30:16 +0100

    Dodan login feature

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello():
 	return render_template('main.html')
 
+@app.route("/")
+def login():
+	return render_template('login.html')
+
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=5000, debug=True)
